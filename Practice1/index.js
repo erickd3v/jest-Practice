30d3f995c1e3a64f0070ca45d5baafd20a545506
erickd3v <moyae76@gmail.com>
2023-12-06 06:11:12 -0500

    Desarrollo de Ruleta

---
+++ Practice1/index.js
@@ -0,0 +1,29 @@
+console.log('Ruleta');
+
+console.log(Math.round(Math.random() * 36));
+
+let price = [
+    '10',
+    '20',
+    '50',
+];
+
+let numberRandom = Math.round(Math.random() * 36);
+
+// Ingresando número para la ruleta
+
+function userSelect(number) {
+    
+    // Function para verificar el precio
+    function game(pen){
+        console.log(price[0])
+    }
+    if(number !== numberRandom) {
+        console.log(numberRandom);
+        console.log('you louse');
+        console.log(game());
+    } else {
+        console.log(numberRandom);
+        console.log('you win');
+    }
+}
