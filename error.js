6378dc70d33425bc5e21ba3f1a970e410471b1f7
erickd3v <moyae76@gmail.com>
2024-01-09 06:44:31 -0500

    feat: Modifications

--- error.js
+++ error.js
@@ -2,13 +2,14 @@ const errNumber = new Error(`Error el tipo de dato no es un número`);
 const errStr = new Error(`Error el tipo de dato no es un string`);
 const errArray = new Error(`Error el tipo de dato no es un array`);
 const errObj = new Error(`Error el tipo de dato no es un object`);
-const error = new Error(`Error, el resultado esperado era: `);
 const errList = new Error(`Error, el valor introducido no se encuentra en la lista`);
+const error = new Error(`Error, el resultado esperado era: `);
 
 module.exports = {
     errNumber,
     errStr,
     errArray,
     errObj,
+    errList,
     error
 }
