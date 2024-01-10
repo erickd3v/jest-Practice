974a74abb04efd03bf9a97e439763fa2a3860520
erickd3v <moyae76@gmail.com>
2024-01-10 17:00:44 -0500

    feat: Added errEmail

--- error.js
+++ error.js
@@ -3,6 +3,7 @@ const errStr = new Error(`Error el tipo de dato no es un string`);
 const errArray = new Error(`Error el tipo de dato no es un array`);
 const errObj = new Error(`Error el tipo de dato no es un object`);
 const errList = new Error(`Error, el valor introducido no se encuentra en la lista`);
+const errEmail = new Error(`Error el texto ingresado no es un email`);
 const error = new Error(`Error, el resultado esperado era: `);
 
 module.exports = {
@@ -11,5 +12,6 @@ module.exports = {
     errArray,
     errObj,
     errList,
+    errEmail,
     error
 }
