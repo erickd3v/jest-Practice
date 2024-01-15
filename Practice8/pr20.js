385dc8c35939fa5128c706d5f71a5eb7d57ae455
erickd3v <moyae76@gmail.com>
2024-01-15 06:58:10 -0500

    refactor: Improved email validation to include top-level domain (TLD) validation

--- Practice8/pr20.js
+++ Practice8/pr20.js
@@ -1,15 +1,24 @@
 const { errStr } = require("../validationErrors");
+/**Forma 1
+ * // const email = ['@gmail.com', '@hotmail.com', '@outlook.com'];
 
-const email = ['@gmail.com', '@hotmail.com', '@outlook.com'];
+   // const isEmail = (text) => {
+   //     if(typeof text === 'undefined') return;
+   //     if(typeof text !== 'string') return errStr;
+   //     return email.some(e => text.endsWith(e));
+   // }
 
-const isEmail = (text) => {
-    if(typeof text === 'undefined') return;
-    if(typeof text !== 'string') return errStr;
-    return email.some(e => text.endsWith(e));
+ */
+
+/**Forma Optimizada para verificar todo tipo de email */
+
+const isEmail = (email) => {
+    if(typeof email === 'undefined') return;
+    if(typeof email !== 'string') return errStr;
+    let expReg = /[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test(email)
+    return expReg;
 }
 
 module.exports = isEmail
 
 console.log(isEmail('erick@outlook.com'));
-console.log(isEmail(2));
-console.log(isEmail());
